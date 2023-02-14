fd9deee9cf6dc212fe8d8089b2bc712a2332038c
abizmo <abizmo@gmail.com>
2023-02-14 15:05:20 +0000

    feat: Rename Main Container

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 
-function MainContainer({ children }: PropsWithChildren) {
+function Container({ children }: PropsWithChildren) {
   return (
     <div className='container lg:bg-white lg:h-[37.5rem] lg:shadow-lg lg:rounded-2xl lg:self-center lg:flex lg:gap-4 lg:p-4'>
       {children}
@@ -8,4 +8,4 @@ function MainContainer({ children }: PropsWithChildren) {
   );
 }
 
-export default MainContainer;
+export default Container;
