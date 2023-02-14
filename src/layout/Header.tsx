140111b3588191fc83e70bf338de05ed9a890980
Abián Izquierdo <abizmo@gmail.com>
2023-02-14 17:43:28 +0000

    fix(Header): remove border-radius on small devices

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 
 function Header({ children }: PropsWithChildren) {
   return (
-    <header className='bg-mobile bg-no-repeat bg-cover lg:bg-desktop p-8 lg:py-10 h-44 lg:h-full lg:w-72 rounded-xl'>
+    <header className='bg-mobile bg-no-repeat bg-cover lg:bg-desktop p-8 lg:py-10 h-44 lg:h-full lg:w-72 lg:rounded-xl'>
       <nav className='w-min mx-auto lg:w-auto' aria-label='Navigation'>
         {children}
       </nav>
